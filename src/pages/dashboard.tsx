d0528d57efd3f06c7050c2ea6d8e9978681491d9
unknown <chromegold70@gmail.com>
2022-01-05 10:42:58 -0300

    Add component permission

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from 'react'
+import { Can } from '../components/Can'
 import { AuthContext } from '../contexts/AuthContext'
 import { useCan } from '../hooks/useCan'
 import { setupAPIClient } from '../services/api'
@@ -24,6 +25,10 @@ export default function Dashboard() {
       <h1>Dashboard: {user?.email}</h1>
 
       {userCanSeeMatrics && <div>Métricas</div>}
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas Component</div>
+      </Can>
     </>
   )
 }
